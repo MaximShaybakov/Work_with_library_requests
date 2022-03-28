27ab1f68ca69256b268844873b4bbd017700834b
Maxim Shaybakov <momon_boton@mail.ru>
2022-03-28 15:21:15 +0500

    Done!

--- work_on_API.py
+++ work_on_API.py
@@ -1,5 +1,4 @@
 import requests
-# from pprint import pprint
 
 class Hero:
 
@@ -9,35 +8,22 @@ class Hero:
     def search_intelligence(self):
         url = f"https://superheroapi.com/api/2619421814940190/search/{self.name}"
         data = requests.post(url, timeout=5)
-        # print(data.json())
         return data.json()
 
     def ent_intel_hero(self):
-        intelligence = int(self.search_intelligence()['results'][0]['powerstats']['intelligence'])
+        self.intelligence = int(self.search_intelligence()['results'][0]['powerstats']['intelligence'])
         # print(self.intelligence)
-        return intelligence
-
-    def __lt__(self, other):
-        if not isinstance(other, Hero):
-            print('Invalid data.')
-            return
-        elif self.ent_intel_hero() > other.ent_intel_hero():
-            print(f'{self.name} intelligence = {self.ent_intel_hero()}.\nHe is smarter!')
-        else:
-            print(f'{other.name} intelligence = {self.ent_intel_hero()}.\nHe is smarter!')
+        return self.intelligence
 
 Thanos = Hero('Thanos')
 Hulk = Hero('Hulk')
 Cap_America = Hero('Captain America')
 
-Thanos.search_intelligence()
-Hulk.search_intelligence()
-Cap_America.search_intelligence()
+def very_smartest(first=Thanos, second=Hulk, third=Cap_America):
+    very_smart = max(first.ent_intel_hero(), second.ent_intel_hero(), third.ent_intel_hero())
+    for i in first,second, third:
+        if i.ent_intel_hero() == very_smart:
+            name = i.name
+    return f"Smartest hero {name}. IQ = {very_smart}"
 
-Thanos.ent_intel_hero()
-Hulk.ent_intel_hero()
-Cap_America.ent_intel_hero()
-
-Hulk > Cap_America
-Thanos > Hulk
-Cap_America > Thanos
+print(very_smartest())
