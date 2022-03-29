c59024e690c3aa8d96ef1148f676088fb36da93c
Maxim Shaybakov <momon_boton@mail.ru>
2022-03-29 11:47:12 +0500

    Finished file.

--- test.py
+++ test.py
@@ -3,17 +3,29 @@ import os
 from pprint import pprint
 
 class YaDiskAPI:
+    '''
+    Класс для работы с яндекс диском.
+    '''
 
     def __init__(self, token):
+        '''
+        Инициализация класса с указанием Вашего токена.
+        '''
         self.token = TOKEN
 
     def get_headers(self):
+        '''
+        Параметры запроса.
+        '''
         return {
             'Content-Type': 'application/json',
             'Authorization': f'OAuth {self.token}'
         }
 
     def get_resp(self):
+        '''
+        Получение списка всех файлов в формате json.
+        '''
         url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
         headers = self.get_headers()
         resp = requests.get(url, headers=headers, timeout=5)
@@ -21,6 +33,9 @@ class YaDiskAPI:
         return resp
 
     def get_upload_link(self, disk_file_path: str):
+        '''
+        Получение пути на яндекс.диск для загрузки Вашего файла.
+        '''
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = self.get_headers()
         params = {"path": disk_file_path, "overwrite": "true"}
@@ -28,7 +43,10 @@ class YaDiskAPI:
         # pprint(response.json())
         return response.json()
 
-    def upload_file_to_disk(self, disk_file_path: str, name: str):
+    def upload_file_to_disk(self, disk_file_path: str):
+        '''
+        Загрузка файла на яндекс диск.
+        '''
         href = self.get_upload_link(disk_file_path=disk_file_path).get("href", "")
         headers = self.get_headers()
         response = requests.put(href, data=open(disk_path_to_file, 'rb'))
@@ -40,16 +58,13 @@ class YaDiskAPI:
 
 
 if __name__ == '__main__':
-    with open('text.txt') as file:
-        TOKEN = file.read()
 
-    # TOKEN = ''
+    TOKEN = '' # введите Ваш токен
 
-    disk_path_to_file = os.path.abspath("") # полный путь к загружаемому файлу
-    disk_file_path = "disk:/search_info.py"
-    name = 'search_info.py'
+    disk_path_to_file = os.path.abspath("...") # указывает полный путь к загружаемому файлу на Вашем ПК
+    disk_file_path = "disk:/..." # вместо "..." введите имя файла с которым он будет сохранён
 
-    test1 = YaDiskAPI(TOKEN)
-    test1.get_resp()
-    test1.get_upload_link(disk_file_path)
-    test1.upload_file_to_disk(disk_file_path, name)
+    test = YaDiskAPI(TOKEN)
+    test.get_resp()
+    test.get_upload_link(disk_file_path)
+    test.upload_file_to_disk(disk_file_path)
